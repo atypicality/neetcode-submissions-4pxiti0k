967413ae2f2165a2e1328febb2d2071dd3c36b72
Dathan Lang <dathan_lang@outlook.com>
2026-06-01 19:31:11 -0400

    Add: string-matching-in-an-array - submission-0

---
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> ans;
+        for(int i = 0; i < words.size(); ++i)
+        {
+            for(int j = 0; j < words.size(); ++j)
+            {
+                if(i == j || words[i].size() > words[j].size()) continue;
+                if(words[j].contains(words[i])) 
+                {
+                    ans.push_back(words[i]);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
